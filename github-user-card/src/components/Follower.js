6521013074a8211190951b30e6ab1dc7997c15b8
Randy Ortiz <Randyortiz28@gmail.com>
2021-03-09 20:41:43 -0500

    Edited some text to uppercase and edited user follower image to have border-radius

--- github-user-card/src/components/Follower.js
+++ github-user-card/src/components/Follower.js
@@ -8,11 +8,11 @@ class Follower extends Component {
         return (
             <div className="user-container">
                 <h1>{follower.login}</h1>
-                    <img src={follower.avatar_url} alt="Randy" />
-                    <h2>id: {follower.id}</h2>
+                    <img className="user-image" src={follower.avatar_url} alt="Randy" />
+                    <h2>Id: {follower.id}</h2>
                     <h2>Bio: {follower.bio ? follower.bio : 'N/A'}</h2>
-                    <h2>follower: {follower.followers ? follower.followers : 'N/A'}</h2>
-                    <h2>following: {follower.following ? follower.following : 'N/A'}</h2>
+                    <h2>Follower: {follower.followers ? follower.followers : 'N/A'}</h2>
+                    <h2>Following: {follower.following ? follower.following : 'N/A'}</h2>
                     <h2>Twitter: {follower.twitter_followername ? follower.twitter_followername : 'N/A'}</h2>
                     <h2>Location: {follower.location ? follower.location : 'N/A'}</h2>
             </div>
