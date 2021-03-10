939519db0b5afe1aa12e89aece0fe2c23a5e8198
Randy Ortiz <Randyortiz28@gmail.com>
2021-03-09 20:37:41 -0500

    Changed background to github background image and added my github contriution chart

--- github-user-card/src/components/UserCard.js
+++ github-user-card/src/components/UserCard.js
@@ -6,16 +6,22 @@ class UserCard extends Component {
         const { user } = this.props;
 
         return (
+            <>
             <div className="user-container">
                 <h1>{user.name}</h1>
-                <img src={user.avatar_url} alt="Randy" />
+                <img className="user-image" src={user.avatar_url} alt="Randy" />
                 <h2>id: {user.id}</h2>
                 <h2>Bio: {user.bio}</h2>
                 <h2>followers: {user.followers}</h2>
                 <h2>following: {user.following}</h2>
                 <h2>Twitter: {user.twitter_username}</h2>
                 <h2>Location: {user.location}</h2>
+                <div className="github-chart">
+                    <h2>GitHub-Contribution</h2>
+                    <img src="http://ghchart.rshah.org/Randyo28" alt="Randyo28's Github chart" />
+                </div>
             </div>
+            </>
         );
     }
 }
